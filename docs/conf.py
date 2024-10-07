f2f341bbfa71c0fbaf756e3821231633dbe30c44
Cadair <1391051+Cadair@users.noreply.github.com>
2024-10-07 07:05:54 +0000

    Automatic package template update

--- docs/conf.py
+++ docs/conf.py
@@ -21,8 +21,9 @@ if _version.is_postrelease:
     version = release = _version.base_version
 # Avoid long githashes in rendered Sphinx docs
 elif _version.is_devrelease:
-    version = release = f'{_version.base_version}.dev{_version.dev}'
+    version = release = f"{_version.base_version}.dev{_version.dev}"
 is_development = _version.is_devrelease
+is_release = not(_version.is_prerelease or _version.is_devrelease)
 
 project = "sunkit-magex"
 author = "The SunPy Community"
@@ -64,7 +65,7 @@ source_suffix = ".rst"
 master_doc = "index"
 
 # Treat everything in single ` as a Python reference.
-default_role = 'py:obj'
+default_role = "py:obj"
 
 # -- Options for intersphinx extension ---------------------------------------
 
