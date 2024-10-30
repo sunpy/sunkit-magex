f09fe60605b5a38db4ad0f4a8a0652867340baab
Samuel Bennett <CyclingNinja@users.noreply.github.com>
2024-10-30 16:44:02 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -80,13 +80,10 @@ intersphinx_mapping = {
     "pfsspy": ("https://pfsspy.readthedocs.io/en/latest/", None),
 }
 
-<<<<<<<
 # -- Options for HTML output -------------------------------------------------
 
-=======
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
->>>>>>>
 html_theme = "sunpy"
 
 # Render inheritance diagrams in SVG
