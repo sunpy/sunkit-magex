a366d08893a400cc031a9cc6845b776b4faf00f9
Trestan Simon <trestansimon@proton.me>
2025-08-20 21:40:00 -0600

    Replace "String" with "str"
    Co-authored-by: Nabil Freij <nabil.freij@gmail.com>

--- sunkit_magex/pfss/input.py
+++ sunkit_magex/pfss/input.py
@@ -23,7 +23,7 @@ class Input:
         on.
     rss : float
         Radius of the source surface, as a fraction of the solar radius.
-    br_outer : sunpy.map.GenericMap, String
+    br_outer : sunpy.map.GenericMap, str
         Boundary condition of radial magnetic field at the outer surface.
 
     Notes
