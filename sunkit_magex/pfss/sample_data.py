5f58d9de26f1ace03d2a3561dd2320a47e846d7f
Stuart Mumford <stuart@cadair.com>
2026-09-09 15:34:22 +0000

    Use sample data from sunpy mirror (#150)

--- sunkit_magex/pfss/sample_data.py
+++ sunkit_magex/pfss/sample_data.py
@@ -6,18 +6,28 @@ using `sunpy.data.manager`.
 from sunpy.data import manager
 
 
-@manager.require('gong_map',
-                 'https://gong2.nso.edu/oQR/zqs/202009/mrzqs200901/mrzqs200901t1304c2234_022.fits.gz',
-                 'aad927d8f617f32b72255b862c4910f13640fc7ca13edf98288cd0735a2db6a0')
+@manager.require(
+    "gong_map",
+    [
+        "https://github.com/sunpy/data/raw/main/sunkit-magex/mrzqs200901t1304c2234_022.fits.gz",
+        "https://gong2.nso.edu/oQR/zqs/202009/mrzqs200901/mrzqs200901t1304c2234_022.fits.gz",
+    ],
+    "aad927d8f617f32b72255b862c4910f13640fc7ca13edf98288cd0735a2db6a0",
+)
 def get_gong_map():
     """
     Automatically download and unzip a sample GONG synoptic map.
     """
-    return manager.get('gong_map')
+    return manager.get("gong_map")
 
 
-@manager.require('adapt_map',
-                 'https://gong.nso.edu/adapt/maps/gong/2020/adapt40311_044012_202010100000_i00005600n1.fts.gz',
-                 'b8a04ec57eb3e797fe3de88f0d2b58b1a93607a914625341bb5a79268cc6314a')
+@manager.require(
+    "adapt_map",
+    [
+        "https://github.com/sunpy/data/raw/main/sunkit-magex/adapt40311_044012_202010100000_i00005600n1.fts.gz",
+        "https://gong.nso.edu/adapt/maps/gong/2020/adapt40311_044012_202010100000_i00005600n1.fts.gz",
+    ],
+    "b8a04ec57eb3e797fe3de88f0d2b58b1a93607a914625341bb5a79268cc6314a",
+)
 def get_adapt_map():
-    return manager.get('adapt_map')
+    return manager.get("adapt_map")
